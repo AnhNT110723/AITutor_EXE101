010b4afd7eeb0fa15e8946a34ed3fb851f0797af
Nguyen Tuan Anh <tuananh2003hy@gmail.com>
2025-06-10 21:40:47 +0700

    anhnt add git ignore

--- EXE-FAIEnglishTutor/EXE-FAIEnglishTutor/Services/Implementaion/ForgotPasswordServiceImpl.cs
+++ EXE-FAIEnglishTutor/EXE-FAIEnglishTutor/Services/Implementaion/ForgotPasswordServiceImpl.cs
@@ -62,7 +62,7 @@ namespace EXE_FAIEnglishTutor.Services.Implementaion
             var emailModel = new EmailVerificationDto
             {
                 FullName = user.FullName,
-                VerificationLink = "http://localhost:5037/Account/ResetPassword?token=" + token + "&Email=" + user.Email,
+                VerificationLink = "https://faienglish.xyz/Account/ResetPassword?token=" + token + "&Email=" + user.Email,
                 ExpirationTime = DateTime.UtcNow.AddMinutes(Constants.EXPIRATION_FORGOTPASSWORD),
                 isResendPassword = true,
                 isVertification = false,
