bc644a7b690c3778bb38d6d7a81dcd70c6a31fe9
Chuchung <chuquockhanhchung@gmail.com>
2025-07-16 15:06:09 +0700

    voucher

---
+++ EXE-FAIEnglishTutor/EXE-FAIEnglishTutor/Controllers/VoucherController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EXE_FAIEnglishTutor.Controllers
+{
+    public class VoucherController : Controller
+    {
+        public IActionResult MusicVoucher()
+        {
+            return View();
+        }
+    }
+}
